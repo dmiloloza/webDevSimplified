1521f236d2e94f5fbb445e4fa27256100f2869b7
Domagoj Miloloza <dmiloloza@github.com>
2020-08-10 15:09:26 +0200

    Inital commit :tada:

--- ES6 Tutorials.js
+++ ES6 Tutorials.js
@@ -1,3 +1,52 @@
+//JavaScript ES6 Arrow Function Tutorial
+function sum(a,b) {
+    return a+b;
+}
+
+let sum2 = (a,b) => a+b
+
+function isPositive(number) {
+    return number >=0;
+}
+
+let isPositive2 = number => number >=0
+
+function randomNumber(){
+    return Math.random
+}
+
+let randomNumber2 = () => Math.random
+
+
+/*document.addEventListener('click', function(){
+    console.log('Click');
+})
+
+document.addEventListener('click', () => console.log('Click'));*/
+
+
+class Person {
+    constructor(name) {
+        this.name = name
+    }
+
+    printNameArrow(){
+        setTimeout(()=>{
+            console.log('Arrow: ' + this.name);
+        },1000)
+    }
+
+    printNameFunction(){
+        setTimeout(function (){
+            console.log('Function: ' + this.name);
+        }, 3000)
+    }
+}
+ let person = new Person('Bob')
+
+person.printNameArrow()
+person.printNameFunction()
+
 //8 Must Know JavaScript Array Methods
 /*
     const items = [
@@ -57,15 +106,6 @@
     const includesTwo = numArray.includes(2)
     console.log(includesTwo)
 */
-//JavaScript ES6 Arrow Function Tutorial
-
-
-
-
-
-
-
-
 
 //JavaScript Promises in 10 min
 /*
